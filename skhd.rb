509d4567543e5677775c2ba19ffd7b6103fedc85
Alok Singh <alokmilenium@gmail.com>
2023-05-26 03:17:30 +0530

    Update skhd.rb

--- skhd.rb
+++ skhd.rb
@@ -1,19 +1,13 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Skhd < Formula
   desc "Xieyt CUSTOM  skhd"
-  homepage "https://github.com/koekeishiya/skhd"
+  homepage "https://github.com/xieyt/skhd"
   url "https://github.com/Xieyt/skhd/archive/refs/tags/v0.2.tar.gz"
   sha256 "e641e97489690e2a6bc805ec9bbf2b75458aed064c1f7ca873ff3dcaaf248352"
-  head "https://github.com/koekeishiya/skhd"
-  license ""
+  head "https://github.com/Xieyt/skhd.git"
 
-  option "with-logging", "Redirect stdout and stderr to log files"
-
-  def install
-    (var/"log/skhd").mkpath
-    system "make", "install"
+   def install
+    ENV.deparallelize
+    system "make", "-j1", "install"
     bin.install "#{buildpath}/bin/skhd"
     (pkgshare/"examples").install "#{buildpath}/examples/skhdrc"
   end
@@ -22,78 +16,15 @@ class Skhd < Formula
     Copy the example configuration into your home directory:
       cp #{opt_pkgshare}/examples/skhdrc ~/.skhdrc
 
-    If the formula has been built with --with-logging, logs will be found in
-      #{var}/log/skhd/skhd.[out|err].log
+    If you want skhd to be managed by launchd (start automatically upon login):
+      skhd --start-service
+
+    When running as a launchd service logs will be found in:
+      /tmp/skhd_<user>.[out|err].log
     EOS
   end
 
-  plist_options :manual => "skhd"
-
-  if build.with? "logging"
-      def plist; <<~EOS
-        <?xml version="1.0" encoding="UTF-8"?>
-        <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-        <plist version="1.0">
-        <dict>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>ProgramArguments</key>
-          <array>
-            <string>#{opt_bin}/skhd</string>
-          </array>
-          <key>EnvironmentVariables</key>
-          <dict>
-            <key>PATH</key>
-            <string>#{HOMEBREW_PREFIX}/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
-          </dict>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>KeepAlive</key>
-          <true/>
-          <key>StandardOutPath</key>
-          <string>#{var}/log/skhd/skhd.out.log</string>
-          <key>StandardErrorPath</key>
-          <string>#{var}/log/skhd/skhd.err.log</string>
-          <key>ProcessType</key>
-          <string>Interactive</string>
-          <key>Nice</key>
-          <integer>-20</integer>
-        </dict>
-        </plist>
-        EOS
-      end
-  else
-      def plist; <<~EOS
-        <?xml version="1.0" encoding="UTF-8"?>
-        <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-        <plist version="1.0">
-        <dict>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>ProgramArguments</key>
-          <array>
-            <string>#{opt_bin}/skhd</string>
-          </array>
-          <key>EnvironmentVariables</key>
-          <dict>
-            <key>PATH</key>
-            <string>#{HOMEBREW_PREFIX}/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
-          </dict>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>KeepAlive</key>
-          <true/>
-          <key>ProcessType</key>
-          <string>Interactive</string>
-          <key>Nice</key>
-          <integer>-20</integer>
-        </dict>
-        </plist>
-        EOS
-      end
-  end
-
   test do
-    assert_match "skhd #{version}", shell_output("#{bin}/skhd --version")
+    assert_match "skhd-v#{version}", shell_output("#{bin}/skhd --version")
   end
 end
